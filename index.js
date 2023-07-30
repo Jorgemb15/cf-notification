08913756415f90c19953f03d568eefcf5094b8ba
Jorgemb15 <jorgemb15@hotmail.es>
2023-07-30 13:01:21 -0600

    master

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
 const nodeMailer = require("nodemailer");
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager').v1;
+const { SecretManagerServiceClient } =
+  require("@google-cloud/secret-manager").v1;
+const fs = require("fs");
 
 exports.execute = async (event, fcontext) => {
-    if (!(event && event.data)) {
-      throw new Error('Invalid payload: event, event.data');
+  if (!(event && event.data)) {
+    throw new Error("Invalid payload: event, event.data");
+  }
+  const payload = JSON.parse(Buffer.from(event.data, "base64").toString());
+
+  console.log(payload);
+
+  const secretmanagerClient = new SecretManagerServiceClient();
+
+  const request = {
+    name: "projects/977716192399/secrets/test/versions/latest",
+  };
+
+  const [response] = await secretmanagerClient.accessSecretVersion(request);
+  const credentials = JSON.parse(response?.payload?.data.toString());
+
+  let transporter = nodeMailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 587,
+    auth: {
+      user: "apikey",
+      pass: credentials.SENDGRID_API_KEY,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const template = fs.readFileSync(`./templates/${payload?.template}`);
+
+  for (const element of payload.recipients) {
+    let content = template.toString();
+    for (const [key, val] of Object.entries(element.params)) {
+      const regex = new RegExp(`@@${key}`, "g");
+      content = content.replace(regex, "" + val);
     }
-    const payload = JSON.parse(Buffer.from(event.data, 'base64').toString());
-
-    console.log(payload);
-
-    const secretmanagerClient = new SecretManagerServiceClient();
-
-    const request = {
-      name: 'projects/977716192399/secrets/test/versions/latest'
-    };
-
-    const [response]  = await secretmanagerClient.accessSecretVersion(request);
-    const credentials = JSON.parse(response?.payload?.data.toString());
-
-    let transporter = nodeMailer.createTransport({
-      host: 'smtp.sendgrid.net',
-      port: 587,
-      auth: {
-          user: "apikey",
-          pass: credentials.SENDGRID_API_KEY
-      },
-      tls: {
-        rejectUnauthorized: false
-      }
-   })
 
     const mailOptions = {
       from: credentials.GMAIL_ADDRESS,
-      to: payload.to,
+      to: element.recipient,
       subject: payload.subject,
-      html: payload.html
+      html: content,
     };
 
+    console.log(content);
+
     transporter
-    .sendMail(mailOptions)
-    .then(() => {
-      console.log('sadf')
-    })
-    .catch(e => {
-      console.log(e)
-    });
-}
+      .sendMail(mailOptions)
+      .then(() => {
+        console.log("Enviado");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+};
--- test.js
+++ test.js
@@ -1,9 +1,22 @@
 const cf = require("./index");
 
 const json = {
-  to: "jorgemb15@hotmail.es",
+  recipients: [
+    {
+      params: {
+        password: "123456",
+      },
+      recipient: "jorgemb15@hotmail.es",
+    },
+    {
+      params: {
+        password: "321654",
+      },
+      recipient: "jorgemb15@hotmail.es",
+    },
+  ],
   subject: "prueba",
-  html: "<h1>GCDCR</h1> <br/> <h3>Esto es una prueba<h3/>",
+  template: "new-password.html",
 };
 
 const data = JSON.stringify(json);
@@ -11,8 +24,6 @@ const data = JSON.stringify(json);
 const dataBuffer = Buffer.from(data);
 
 const event = { data: dataBuffer };
-
-console.log(event.data);
 
 console.log(JSON.stringify(event.data));
 
